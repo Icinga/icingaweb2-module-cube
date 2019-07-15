5bacc9cfafa8a09f58faa2979ab58d404f8953a6
Eric Lippmann <eric.lippmann@icinga.com>
2019-07-15 10:17:26 +0200

    Add action links for service cubes

--- library/Cube/ProvidedHook/Cube/MonitoringActions.php
+++ library/Cube/ProvidedHook/Cube/MonitoringActions.php
@@ -6,34 +6,47 @@ namespace Icinga\Module\Cube\ProvidedHook\Cube;
 use Icinga\Module\Cube\Hook\ActionsHook;
 use Icinga\Module\Cube\Cube;
 use Icinga\Module\Cube\Ido\IdoHostStatusCube;
+use Icinga\Module\Cube\Ido\IdoServiceStatusCube;
 use Icinga\Web\View;
 
 /**
  * MonitoringActionLinks
  *
- * An action link hook implementation linking to matching hosts in the
+ * An action link hook implementation linking to matching hosts/services in the
  * monitoring module
  */
 class MonitoringActions extends ActionsHook
 {
     public function prepareActionLinks(Cube $cube, View $view)
     {
-        if (! $cube instanceof IdoHostStatusCube) {
-            return;
+        if ($cube instanceof IdoHostStatusCube) {
+            $vars = [];
+            foreach ($cube->getSlices() as $key => $val) {
+                $vars['_host_' . $key] = $val;
+            }
+
+            $url = 'monitoring/list/hosts';
+
+            $this->addActionLink(
+                $this->makeUrl($url, $vars),
+                $view->translate('Show hosts status'),
+                $view->translate('This shows all matching hosts and their current state in the monitoring module'),
+                'host'
+            );
+        } elseif ($cube instanceof IdoServiceStatusCube) {
+            $vars = [];
+            foreach ($cube->getSlices() as $key => $val) {
+                $vars['_service_' . $key] = $val;
+            }
+
+            $url = 'monitoring/list/services';
+
+            $this->addActionLink(
+                $this->makeUrl($url, $vars),
+                $view->translate('Show services status'),
+                $view->translate('This shows all matching services and their current state in the monitoring module'),
+                'host'
+            );
         }
-
-        $vars = array();
-        foreach ($cube->getSlices() as $key => $val) {
-            $vars['_host_' . $key] = $val;
-        }
-
-        $url = 'monitoring/list/hosts';
-
-        $this->addActionLink(
-            $this->makeUrl($url, $vars),
-            $view->translate('Show hosts status'),
-            $view->translate('This shows all matching hosts and their current state in the monitoring module'),
-            'host'
-        );
     }
 }
