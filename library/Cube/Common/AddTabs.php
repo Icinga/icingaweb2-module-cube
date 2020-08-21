1f6b6b14f3441958dbf8cc7ea4db8f3c2e6fefde
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2020-08-21 15:40:35 +0200

    Fix : change tabs (host, service) with existing params

--- library/Cube/Common/AddTabs.php
+++ library/Cube/Common/AddTabs.php
@@ -8,16 +8,19 @@ trait AddTabs
 {
     public function addTabs($active)
     {
+        $param = '';
+        if (! empty(Url::fromRequest()->getParams()->toString())) {
+            $param = '?' . Url::fromRequest()->getParams()->toString();
+        }
         $this->getTabs()->add('hosts', new Tab([
             'title' => 'hosts',
-            'url' => Url::fromPath('cube/icingadb/hosts'),
+            'url' => Url::fromPath('cube/icingadb/hosts' . $param),
         ]));
 
         $this->getTabs()->add('services', new Tab([
             'title' => 'services',
-            'url' => Url::fromPath('cube/icingadb/services'),
+            'url' => Url::fromPath('cube/icingadb/services' . $param),
         ]));
-
 
         $this->getTabs()->activate($active);
     }
