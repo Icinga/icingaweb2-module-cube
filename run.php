46920b8fd575f9358ce0b51954316b1f45a27131
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2020-12-11 14:43:30 +0100

    Provide IcingaDbStatus hook

--- run.php
+++ run.php
@@ -2,3 +2,4 @@
 // Icinga Web 2 Cube Module | (c) 2016 Icinga GmbH | GPLv2
 
 $this->provideHook('cube/Actions', 'Cube/MonitoringActions');
+$this->provideHook('cube/Icingadb', 'Cube/IcingadbStatus');
