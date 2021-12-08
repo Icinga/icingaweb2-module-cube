a2eca39fc10eb7170c6ca55a06767982680e3960
raviks789 <33730024+raviks789@users.noreply.github.com>
2021-12-08 09:48:13 +0100

    Replace HostCube with IcingadbHostStatusCube for host cubes.

--- application/controllers/HostsController.php
+++ application/controllers/HostsController.php
@@ -4,9 +4,7 @@
 namespace Icinga\Module\Cube\Controllers;
 
 use Icinga\Application\Modules\Module;
-use Icinga\Module\Cube\DimensionParams;
-use Icinga\Module\Cube\HostCube;
-use Icinga\Module\Cube\HostDbQuery;
+use Icinga\Module\Cube\Icingadb\IcingadbHostStatusCube;
 use Icinga\Module\Cube\ProvidedHook\Icingadb\IcingadbSupport;
 use Icinga\Module\Cube\Ido\IdoHostStatusCube;
 use Icinga\Module\Cube\Web\Controller;
@@ -21,7 +19,7 @@ class HostsController extends Controller
     }
 
     /**
-     * @return HostCube|IdoHostStatusCube
+     * @return IcingadbHostStatusCube|IdoHostStatusCube
      */
     protected function getCube()
     {
@@ -29,25 +27,6 @@ class HostsController extends Controller
             return new IdoHostStatusCube();
         }
 
-        $slices = [];
-        $urlDimensions = DimensionParams::fromString($this->params->get('dimensions'))->getDimensions();
-
-        $dimensionsWithoutSlices = $urlDimensions;
-        // get slices
-        foreach ($urlDimensions as $key => $dimension) {
-            // because params are double encoded
-            $doubleEncodedDimension = DimensionParams::update(rawurlencode($dimension))->getParams();
-
-            if ($value = $this->params->get($doubleEncodedDimension)) {
-                unset($dimensionsWithoutSlices[$key]);
-                $slices[$dimension] = $value;
-            }
-        }
-
-        return (new HostCube(
-            new HostDbQuery(),
-            $urlDimensions,
-            $slices
-        ));
+        return (new IcingadbHostStatusCube());
     }
 }
