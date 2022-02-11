7aefebf7bcec81600cf32db10625f6bbd338bc74
Johannes Meyer <johannes.meyer@icinga.com>
2022-02-11 12:02:28 +0100

    HoststatusQuery: Ensure compatibility with legacy Web versions
    Fixed in v2.10 [here](https://github.com/Icinga/icingaweb2/pull/4671).

--- library/Cube/Ido/Query/HoststatusQuery.php
+++ library/Cube/Ido/Query/HoststatusQuery.php
@@ -3,6 +3,10 @@
 
 namespace Icinga\Module\Cube\Ido\Query;
 
+use Exception;
+use Icinga\Application\Version;
+use Icinga\Data\Filter\FilterExpression;
+use Icinga\Exception\NotImplementedError;
 use Icinga\Module\Monitoring\Backend\Ido\Query\IdoQuery;
 
 class HoststatusQuery extends \Icinga\Module\Monitoring\Backend\Ido\Query\HoststatusQuery
@@ -20,5 +24,23 @@ class HoststatusQuery extends \Icinga\Module\Monitoring\Backend\Ido\Query\Hostst
         }
 
         return parent::joinSubQuery($query, $name, $filter, $and, $negate, $additionalFilter);
+    }
+
+    protected function createSubQueryFilter(FilterExpression $filter, $queryName)
+    {
+        try {
+            return parent::createSubQueryFilter($filter, $queryName);
+        } catch (Exception $e) {
+            if (version_compare(Version::VERSION, '2.10.0', '>=')) {
+                throw $e;
+            }
+
+            if ($e->getMessage() === 'Undefined array key 0' && basename($e->getFile()) === 'IdoQuery.php') {
+                // Ensures compatibility with earlier Icinga Web 2 versions
+                throw new NotImplementedError('');
+            } else {
+                throw $e;
+            }
+        }
     }
 }
