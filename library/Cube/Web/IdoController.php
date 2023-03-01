b0465adcdfd926fa739f46c804616d47628a3a8d
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2023-03-01 15:52:48 +0100

    Convert params when switching from icingadb to monitoring

--- library/Cube/Web/IdoController.php
+++ library/Cube/Web/IdoController.php
@@ -6,11 +6,15 @@ namespace Icinga\Module\Cube\Web;
 
 use Icinga\Module\Cube\DimensionParams;
 use Icinga\Module\Cube\Forms\DimensionsForm;
+use Icinga\Module\Cube\IcingaDb\CustomVariableDimension;
+use Icinga\Module\Cube\IcingaDb\IcingaDbCube;
 use Icinga\Module\Cube\Ido\IdoCube;
 use Icinga\Web\View;
 use Icinga\Web\Widget\Tabextension\DashboardAction;
 use Icinga\Web\Controller;
 use Icinga\Web\Widget\Tabs;
+use ipl\Stdlib\Str;
+use ipl\Web\Url;
 
 abstract class IdoController extends Controller
 {
@@ -19,6 +23,9 @@ abstract class IdoController extends Controller
 
     /** @var IdoCube */
     protected $cube;
+
+    /** @var bool Whether showSettings param is set */
+    protected $showSettings;
 
     /**
      * Return this controllers' cube
@@ -31,6 +38,9 @@ abstract class IdoController extends Controller
     {
         $this->cube = $this->getCube();
         $this->cube->chooseFacts(array_keys($this->cube->getAvailableFactColumns()));
+
+        $this->showSettings = $this->params->shift('showSettings');
+        $this->prepareCube();
     }
 
     public function detailsAction(): void
@@ -39,8 +49,6 @@ abstract class IdoController extends Controller
             'label' => $this->translate('Cube details'),
             'url'   => $this->getRequest()->getUrl()
         ])->activate('details');
-
-        $this->prepareCube();
 
         $this->view->title = $this->cube->getSlicesLabel();
 
@@ -51,10 +59,6 @@ abstract class IdoController extends Controller
 
     protected function renderCube(): void
     {
-        // Hint: order matters, we are shifting!
-        $showSettings = $this->params->shift('showSettings');
-        $this->prepareCube();
-
         $this->view->title = sprintf(
             $this->translate('Cube: %s'),
             $this->cube->getPathLabel()
@@ -63,10 +67,10 @@ abstract class IdoController extends Controller
         if (count($this->cube->listDimensions()) > 0) {
             $this->view->cube = $this->cube;
         } else {
-            $showSettings = true;
+            $this->showSettings = true;
         }
 
-        if ($showSettings) {
+        if ($this->showSettings) {
             $this->view->form = (new DimensionsForm())->setCube($this->cube);
             $this->view->form->handleRequest();
         } else {
@@ -79,6 +83,11 @@ abstract class IdoController extends Controller
     private function prepareCube(): void
     {
         $vars = DimensionParams::fromString($this->params->shift('dimensions', ''))->getDimensions();
+
+        if ($this->hasIcingadbDimensionParams($vars)) {
+            $this->transformicingadbDimensionParamsAndRedirect($vars);
+        }
+
         $wantNull = $this->params->shift('wantNull');
 
         foreach ($vars as $var) {
@@ -96,6 +105,77 @@ abstract class IdoController extends Controller
             $this->cube->slice(rawurldecode($param[0]), rawurldecode($param[1]));
         }
     }
+
+    /**
+     * Get whether the dimensions contain icingadb dimension
+     *
+     * @param array $dimensions
+     *
+     * @return bool
+     */
+    private function hasIcingadbDimensionParams(array $dimensions): bool
+    {
+        $prefix = CustomVariableDimension::HOST_PREFIX;
+        if ($this->getRequest()->getControllerName() === 'ido-services') {
+            $prefix = CustomVariableDimension::SERVICE_PREFIX;
+        }
+
+        foreach ($dimensions as $dimension) {
+            if (Str::startsWith($dimension, $prefix)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * Transform icingadb dimension and slice params and redirect
+     *
+     * This remove the new icingadb prefix from params and remove sort, problems-only, filter params
+     *
+     * @param array $icingadbDimensions
+     */
+    private function transformIcingadbDimensionParamsAndRedirect(array $icingadbDimensions): void
+    {
+        $dimensions = [];
+        $slices = [];
+        $toRemoveSlices = [];
+
+        $prefix = CustomVariableDimension::HOST_PREFIX;
+        if ($this->getRequest()->getControllerName() === 'ido-services') {
+            $prefix = CustomVariableDimension::SERVICE_PREFIX;
+        }
+
+        foreach ($icingadbDimensions as $param) {
+            $newParam = $param;
+            if (strpos($param, $prefix) !== false) {
+                $newParam = substr($param, strlen($prefix));
+            }
+
+            $slice = $this->params->shift(IcingaDbCube::SLICE_PREFIX . $param);
+            if ($slice) {
+                $slices[$newParam] = $slice;
+                $toRemoveSlices[] = IcingaDbCube::SLICE_PREFIX . $param;
+            }
+
+            $dimensions[] = $newParam;
+        }
+
+        $icingadbParams = array_merge(
+            $icingadbDimensions,
+            $toRemoveSlices,
+            array_keys($this->params->toArray(false))
+        );
+
+        $this->redirectNow(
+            Url::fromRequest()
+                ->setParam('dimensions', DimensionParams::fromArray($dimensions)->getParams())
+                ->addParams($slices)
+                ->without($icingadbParams)
+        );
+    }
+
 
     public function createTabs(): Tabs
     {
