b1c1345b92d4f8d2e9e8830c2b8e8f49eb045d5f
Sukhwinder Dhillon <sukhwinder33445@gmail.com>
2023-08-15 11:16:46 +0200

    Controllers: Don't pass `format` param to db query

--- library/Cube/Web/Controller.php
+++ library/Cube/Web/Controller.php
@@ -90,6 +90,7 @@ abstract class Controller extends CompatController
             $cube->getPathLabel()
         ));
 
+        $this->params->shift('format');
         $showSettings = $this->params->shift('showSettings');
 
         $query = $cube->innerQuery();
