695eafd3e19b1d5271f05eef1f6dcc6b3a1bca8b
Johannes Meyer <johannes.meyer@icinga.com>
2025-03-20 16:56:10 +0100

    configuration.php: Define a icon and priority for the menu integration
    Both are synchronized with the monitoring and reporting module now.
    No matter which one is enabled, the section looks always the same.

--- configuration.php
+++ configuration.php
@@ -2,4 +2,5 @@
 
 // Icinga Web 2 Cube Module | (c) 2016 Icinga GmbH | GPLv2
 
-$this->menuSection(N_('Reporting'))->add($this->translate('Cube'))->setUrl('cube/hosts')->setPriority(10);
+$this->menuSection(N_('Reporting'), ['icon' => 'fa-chart-simple', 'priority' => 100])
+    ->add($this->translate('Cube'))->setUrl('cube/hosts')->setPriority(10);
